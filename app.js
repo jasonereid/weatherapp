0485a74fd869e6046038baf80b647e142782d95d
Jason Reid <94079137+jasonereid@users.noreply.github.com>
2024-01-09 10:21:05 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
-let lat;
-let long;
+async function getApi() {
+    let city = prompt("City name");
+    const weatherApi = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=28fe7b5f9a78838c639143fc517e4343');
+    const data = await weatherApi.json();
+    return(data);
+}
 
+async function runFuncs() {
+    func1 = await getApi();
+    func2 = await updateDom();
+    // log data for testing
+    console.log(func1); 
+}
 
-const successCallback = (position) => {
-    lat = position.coords.latitude;
-    long = position.coords.longitude;
-    console.log(position.coords); 
-    return(lat,long)    
-
-    // call the api to get weather info
-
-    
-  };
-  
-  const errorCallback = (error) => {
-    console.log(error);
-  };
-  
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
-
-
-async function apifunc() {
-    const apiResponse = await fetch("https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + long + "&current=temperature_2m,is_day,precipitation,cloud_cover,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,rain,snowfall&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago&forecast_days=1");
-    let data = await apiResponse.json();
-    console.log(data);
+async function updateDom() {
     let temp = document.getElementById("temp");
     let cloudcover = document.getElementById("cloudcover");
     let precipitation = document.getElementById("precipitation");
     let windspeed = document.getElementById("windspeed");
-    let location = document.getElementById("location");
-    temp.innerHTML = data.current.temperature_2m;
-    cloudcover.innerHTML = data.current.cloud_cover;
-    precipitation.innerHTML = data.current.precipitation;
-    windspeed.innerHTML = data.current.wind_speed_10m;
+    let message = document.getElementById("message");
+    // update divs with new data
+    temp.innerHTML = func1.main.temp;
+        if(func1.main.temp < 50) {
+           temp.innerHTML += " &#10052;"
+        }
+        if(func1.main.temp > 75) {
+            temp.innerHTML += " &#x1F31E;"
+         }
+    cloudcover.innerHTML = func1.clouds.all;
+        if(func1.clouds.all > 50) {
+            cloudcover.innerHTML += " &#9729;"
+        }
+    precipitation.innerHTML = func1.main.humidity;
+        if(func1.main.humidity > 50) {
+            precipitation.innerHTML += " &#128167;"
+        }
+    windspeed.innerHTML = func1.wind.speed; 
+    if(func1.wind.speed > 2) {
+        windspeed.innerHTML += " &#x2634;"
+    }
+    message.innerText = func1.name.toUpperCase();
 }
-    
 
-apifunc();
-
-
+runFuncs();
